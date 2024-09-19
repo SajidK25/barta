747e9d02bf227e5ed6125dd0845642b875bfd348
sajid <sajidk25@gmail.com>
2024-09-19 19:23:23 +0600

    initial commit

---
+++ barta-app/storage/framework/views/ffa1dcc97e27b30f19bf008af5887643.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $__env->yieldContent('title','Barta Home'); ?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- AlpineJS CDN -->
+    <script
+      defer
+      src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <link
+      rel="preconnect"
+      href="https://fonts.googleapis.com" />
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+      rel="stylesheet" />
+
+    <style>
+      * {
+        font-family: 'Inter', sans-serif;
+      }
+    </style>
+  </head>
+<body class="bg-gray-100">
+<header>
+    <?php echo $__env->yieldContent('header_content'); ?>
+</header>
+<main class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
+    <?php echo $__env->yieldContent('main_content'); ?>
+</main">
+<footer class="shadow bg-black mt-10">
+    <?php echo $__env->yieldContent('footer_content'); ?>
+</footer>
+</body>
+</html><?php /**PATH C:\laragon\www\barta\barta-app\resources\views/layouts/layout.blade.php ENDPATH**/ ?>
